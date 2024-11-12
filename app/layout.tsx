1a9b078f0bebb9eeaed5cef7ff984678323f3742
Jack O'Brien <thejackobrien29@gmail.com>
2024-11-12 17:47:15 -0500

    really pretty dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata, Viewport } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import { PT_Mono } from 'next/font/google';
 import { ConvexClientProvider } from './ConvexClientProvider';
 import { Toaster } from '@/components/ui/toaster';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 const ptMono = PT_Mono({
   subsets: ['latin'],
